8e36718db340b0c8e75c5e1e5261140f608efd99
AndresArcioni <andresarcioni@gmail.com>
2020-12-05 16:51:44 -0300

    mercadopago en controller fuera de routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mercadopago = require('mercadopago');
 
 const indexController = require("../controllers/indexController");
-
-
-mercadopago.configure({
-    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
-    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
-})
-
 
 /* GET home page. */
 router.get('/', indexController.home);
